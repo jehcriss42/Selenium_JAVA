0ee459c873028d26ac55136d5bd3df021006d942
Jessica Tavares <jehcriss42@gmail.com>
2019-07-18 23:43:52 -0300

    Added code to Submit login.

--- src/test/java/tests/SignTest.java
+++ src/test/java/tests/SignTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +12,12 @@ public class SignTest {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
         WebDriver browser = new ChromeDriver();
         browser.get("http://automationpractice.com/");
+        browser.findElement(By.className("login")).click();
+        browser.findElement(By.id("email")).sendKeys("aaa@jjj.com");
+        browser.findElement(By.id("passwd")).sendKeys("12345");
+        browser.findElement(By.id("SubmitLogin")).click();
+
+
 
     }
 }
