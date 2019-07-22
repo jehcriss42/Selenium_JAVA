65bc9551c35968e330558f3f53f6edb9b0228fd7
Jessica Tavares <jehcriss42@gmail.com>
2019-07-21 23:18:03 -0300

    Finished TestLogin().

--- src/test/java/tests/SignTest.java
+++ src/test/java/tests/SignTest.java
@@ -1,23 +1,27 @@
 package tests;
 
-import org.junit.Assert;
+import static junit.framework.TestCase.assertEquals;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class SignTest {
     @Test
-    public void testLogin(){
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+    public void testLogin() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
         WebDriver browser = new ChromeDriver();
+
         browser.get("http://automationpractice.com/");
         browser.findElement(By.className("login")).click();
         browser.findElement(By.id("email")).sendKeys("aaa@jjj.com");
         browser.findElement(By.id("passwd")).sendKeys("12345");
         browser.findElement(By.id("SubmitLogin")).click();
 
-
-
+        String checklogin = browser.findElement(By.className("account")).getText();
+        assertEquals("Jessica Tavares", checklogin);
     }
+
 }
