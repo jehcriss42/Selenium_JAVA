15f3978825b749a32c68b3c0281167d9c2adbb7a
Jessica Tavares <jehcriss42@gmail.com>
2019-07-24 23:03:43 -0300

    Added Setup class

---
+++ src/test/java/tests/Setup.java
@@ -0,0 +1,28 @@
+package tests;
+
+/*
+   Created by: Jessica Cristina Tavares
+   Email: jeh_criss@hotmail.com
+   2019
+*/
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Setup {
+
+    static WebDriver browser;
+
+    @BeforeClass
+    public static void BrowserOpen() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        browser = new ChromeDriver();
+    }
+
+    @AfterClass
+    public static void teardown(){
+        browser.quit();
+    }
+}
--- src/test/java/tests/SingIn.java
+++ src/test/java/tests/SingIn.java
@@ -2,54 +2,36 @@ package tests;
 /*
    Created by: Jessica Cristina Tavares
    Email: jeh_criss@hotmail.com
-   Date: 24-jul-19
+   2019
 */
 
 import static junit.framework.TestCase.*;
-import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SingIn {
-
-    /* TODO: initialize drivers and property in different class */
-
-    private String url = "http://automationpractice.com/";
-    private WebDriver browser = new ChromeDriver();
-
-    static {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-    }
+public class SingIn extends Setup {
 
     @Test
     public void testLogin() {
-        browser.get(url);
+        browser.get("http://automationpractice.com/");
         browser.findElement(By.className("login")).click();
         browser.findElement(By.id("email")).sendKeys("aaa@jjj.com");
         browser.findElement(By.id("passwd")).sendKeys("12345");
         browser.findElement(By.id("SubmitLogin")).click();
-
         String checklogin = browser.findElement(By.className("account")).getText();
         assertEquals("Jessica Tavares", checklogin);
+        // Teardown - logout
+        browser.findElement(By.className("logout")).click();
     }
 
     @Test
-    public void testInvalidLogin(){
-        browser.get(url);
+    public void testInvalidLogin() {
+        browser.get("http://automationpractice.com/");
         browser.findElement(By.className("login")).click();
         browser.findElement(By.id("email")).sendKeys("invalid");
         browser.findElement(By.id("passwd")).sendKeys("12345");
         browser.findElement(By.id("SubmitLogin")).click();
-
         String checklogin = browser.findElement(By.cssSelector("div[class='alert alert-danger']")).getText();
         assertTrue(checklogin.contains("Invalid email address"));
     }
-
-    @After
-    public void teardown(){
-        browser.quit();
-    }
-
 }
