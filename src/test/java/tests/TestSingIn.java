ecce52fbf3bc9b4909769ac294e863817dc1ed19
Jessica Tavares <jehcriss42@gmail.com>
2019-08-10 01:21:06 -0300

    added access to login so it can be reused

--- src/test/java/tests/TestSingIn.java
+++ src/test/java/tests/TestSingIn.java
@@ -4,26 +4,12 @@ package tests;
  * Test using Page Object Model (this test calls package pages).
  */
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import static junit.framework.TestCase.*;
 import pages.Login;
 
 
-public class TestSingIn {
-    static WebDriver browser;
-    Login login = new Login(browser);
-
-    @BeforeClass
-    public static void setup(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        browser = new ChromeDriver();
-        browser.get("http://automationpractice.com");
-    }
-
+public class TestSingIn extends Setup {
     /**
 
      * The test cases will reach http://automationpractice.com
@@ -34,8 +20,12 @@ public class TestSingIn {
 
      */
 
+    Login login = new Login(browser);
+
     @Test
     public void testLogin() {
+        browser.get("http://automationpractice.com");
+        login.accessLogin();
         login.loginToSite("aaa@jjj.com","12345");
         String checkLogin = login.loginTitleAccount();
         assertEquals("Jessica Tavares",checkLogin);
@@ -47,11 +37,4 @@ public class TestSingIn {
         String checkLogout = login.loginTitleLogIn();
         assertEquals("Sign in",checkLogout);
     }
-
-    @AfterClass
-    public static void teardown(){
-        browser.quit();
-    }
-
-
 }
